0e481cd3078c36b48894902a99018be7f64e8877
shannonjanehogan <shannon.jane.hogan@gmail.com>
2017-09-30 16:11:28 -0700

    Refactor helper methods into javascript

---
+++ room_sorter_script/RoomSorterHelpers.js
@@ -0,0 +1,107 @@
+const Debater = require('./Debaters');
+const { Team, TeamSkillEnum } = require('.Teams');
+
+const find_random = (group) => {
+  return group[Math.floor(Math.random() * group.length)];
+}
+
+// When extra teams, remove pro teams first, then novs, then proam, then worlds from debating
+const remove_team = (teams_pro, teams_nov, teams_proam, teams_adv) => {
+  let group;
+  if (teams_pro.length > 0) {
+      group = teams_pro;
+  } else if (teams_nov.length > 0) {
+      group = teams_nov;
+  } else if (teams_proam.length > 0) {
+      group = teams_proam;
+  } else (teams_adv.length > 0) {
+      group = teams_adv;
+  }
+}
+
+// Add team to team list
+const add_team = (teams_adv, teams_pro, teams_proam, teams_nov, team) => {
+  let group;
+  if (team.skill == TeamSkill.WORLDS) {
+    group = teams_adv
+  } else if (team.skill === TeamSkill.PRO) {
+    group = teams_pro
+  } else if (team.skill === TeamSkill.PROAM) {
+    group = teams_proam
+  } else (team.skill === TeamSkill.NOV) {
+    group = teams_nov;
+  }
+  if (!group.has(team)) {
+    group.add(team);
+  }
+}
+
+// Create team with specified debaters (called with partner_pref)
+const create_team = (teams_adv, teams_pro, teams_proam, teams_nov, d1, g1, d2, g2) => {
+  const team = new Team(d1, d2);
+  g1.delete(d1);
+  g2.delete(d2);
+  add_team(teams_adv, teams_pro, teams_proam, teams_nov, team);
+}
+
+// Create team with random debaters (called with no partner_pref)
+const create_team_random = (teams_adv, teams_pro, teams_proam, teams_nov, g1, g2) => {
+  const d1 = find_random(g1);
+  g1.delete(d1);
+  const d2 = find_random(g2);
+  g2.delete(d2);
+  const team = new Team(d1, d2);
+  add_team(teams_adv, teams_pro, teams_proam, teams_nov, team);
+}
+
+// Set length to whichever of pro or nov debaters is shorter, then create that number of random pro-am teams
+const handle_proam = (debaters_pro, debaters_nov, teams_adv, teams_pro, teams_proam, teams_nov) => {
+  let length;
+  if (debaters_pro.length < debaters_nov.length) {
+    length = debaters_pro.length;
+  } else {
+    length = debaters_nov.length;
+  }
+  for (let i = 0, i < length, i++) {
+    create_team_random(teams_adv, teams_pro, teams_proam, teams_nov, debaters_pro, debaters_nov);
+  }
+}
+
+// Create full rooms while there are enough teams
+const make_rooms_full = (rooms, judges, sorted_rooms, team_group) => {
+  while (team_group.length >= 4) {
+    const room = find_random(room);
+    rooms.delete(room);
+    room.og = find_random(team_group);
+    team_group.delete(room.og);
+    room.oo = find_random(team_group);
+    team_group.delete(room.oo);
+    room.cg = find_random(team_group);
+    team_group.delete(room.cg);
+    room.co = find_random(team_group);
+    team_group.delete(room.co);
+    room.full = true;
+    const judge = find_random(judges);
+    judges.delete(judge);
+    room.judges.add(judge);
+    room.calc_status();
+    sorted_rooms.add(room);
+  }
+}
+
+// Create half rooms while there are enough teams
+const make_rooms_half = (rooms, judges, sorted_rooms, team_group) => {
+  while (team_group.length >= 2) {
+    const room = find_random(room);
+    rooms.delete(room);
+    room.og = find_random(team_group);
+    team_group.delete(room.og);
+    room.oo = find_random(team_group);
+    team_group.delete(room.oo);
+    const judge = find_random(judges);
+    judges.delete(judge);
+    room.judges.add(judge);
+    room.calc_status();
+    sorted_rooms.add(room);
+  }
+}
